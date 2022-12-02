c762336dee757c5993b9b2bfc658d0069388993f
Tiny Contest Winners <tinycontestwinners@gmail.com>
2022-12-02 14:48:31 -0500

    version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='tcw-tasks',
-    version='0.0.5',
+    version='0.0.6',
     author='J Leary',
     author_email='tinycontestwinners@gmail.com',
     description='contest cleanup script for tcw app',
