614f40540a4fd5612019281613de58756da1ffac
Tiny Contest Winners <tinycontestwinners@gmail.com>
2022-12-06 21:09:24 -0500

    another fix to sendgrid status handler

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='tcw-tasks',
-    version='0.0.8',
+    version='0.0.9',
     author='J Leary',
     author_email='tinycontestwinners@gmail.com',
     description='contest cleanup script for tcw app',
